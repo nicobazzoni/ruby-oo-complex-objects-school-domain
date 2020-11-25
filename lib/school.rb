86b67e67b8609e60b0d34ab827fa23c91d6ec704
Nico Bazzoni <you@example.com>
2020-11-24 21:49:43 -0500

    commit tree aee2ccf3004a89584a25ab64c26e3f42677185b7

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,44 @@
-# code here!
+class School
+    attr_reader :name, :roster
+  
+ def initialize(name)
+  @name = name
+  @roster = {}
+ end
+ 
+ def add_student(student, level)
+
+    roster[level] ||= []
+    roster[level] << student
+  end 
+  
+  
+def sort 
+  nu_hash = {}
+  roster.each do |x, y| 
+    nu_hash[x] = y.sort 
+  end 
+  nu_hash
+end 
+  
+
+ 
+  def add_student(name, grade)
+      @student_name = name
+      @grade = grade
+     if @roster.include?(grade) == false
+       @roster[grade] = []
+  end
+   roster[grade] <<name
+ end
+ def grade(number)
+     @roster[number]
+ end
+
+ def sort
+     @roster.each do |grade, name|
+       @roster[grade] = name.sort
+     end
+    end
+end
+ 
